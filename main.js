368817bb8ee3b3a48c57f25bac6fb03a0f07682d
Ryan <ryan@burgettweb.net>
2016-08-15 07:36:28 -0400

    Adde socket.io

--- main.js
+++ main.js
@@ -17,6 +17,10 @@ const app = express()
     .use(cors())
     .use(express.static('./public'));
 
+// Configure Socket.io
+// const http = require('http').Server(app);
+
+
 // Configure JSON API
 app.get('/names', (req, res) => {
     namesDB.find({}, (err, docs) => {
@@ -46,4 +50,39 @@ app.post('/names', (req, res) => {
 const port = 3300;
 const server = app.listen(port, () => {
     console.log('App listening at port', server.address().port);
+});
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    console.log('Connection!');
+
+    socket.on('getNames', () => {
+        namesDB.find({}, (err, docs) => {
+            if(err) {
+                console.error(err);
+            } else {
+                io.emit('names', docs);
+            }
+        });
+    });
+
+    socket.on('addName', name => {
+        const newDoc = {
+            name: name
+        };
+        namesDB.insert(newDoc, err => {
+            if(err) {
+                console.error(err);
+            } else {
+                namesDB.find({}, (err, docs) => {
+                    if(err) {
+                        console.error(err);
+                    } else {
+                        io.emit('names', docs);
+                    }
+                });
+            }
+        });
+    });
+
 });
